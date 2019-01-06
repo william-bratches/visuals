f60e67e12c64b668bc3f6ee86938ca0d045c55ed
william-bratches <william.bratches@gmail.com>
2019-01-06 17:03:35 -0500

    dynamic loading html

--- crt/lib/loadFile.js
+++ crt/lib/loadFile.js
@@ -1,11 +1,19 @@
-const loadFile = (path) => {
+const getHtmlString = (path) => {
   return Promise.resolve('<div class="cursor">Hi there!</div>');
 }
-  // automatically get current context
-  // document.getElementByTagName("script");
-  // fetch(`/loadFile?path=${path}&context=${context}`).then(res => res.json())
-  // .then(result => {
-  //   return Promise.resolve('<div class="cursor">Hi there!</div>');
-  // });
+
+const loadIntoDocument = (className, html) => {
+  console.log(html);
+  const nodeList = document.getElementsByClassName(className);
+  for (let element = 0; element < nodeList.length; element++) {
+    nodeList[element].innerHTML = html;
+  }
+};
+
+const loadFile = (className, path) => {
+  fetch(path).then(res => res.text())
+  .then(html => loadIntoDocument(className, html));
+};
+  
 
   export default loadFile;
--- crt/views/newYear/weekTracker/weekTracker.js
+++ crt/views/newYear/weekTracker/weekTracker.js
@@ -1,8 +1,3 @@
 import loadFile from '../../../lib/loadFile.js';
 
-loadFile().then((html) => {
-  const nodeList = document.getElementsByClassName('week-tracker');
-  for (let element = 0; element < nodeList.length; element++) {
-    nodeList[element].innerHTML = html;
-  }
-});
+loadFile('week-tracker', '/views/newYear/weekTracker/weekTracker.html');
