f780d23d6a02e6fa1082f6b9cc3c5ea045de4c1e
william-bratches <william.bratches@gmail.com>
2019-01-09 18:44:02 -0500

    wip filling in data

--- crt/views/newYear/weekTracker/weekTracker.js
+++ crt/views/newYear/weekTracker/weekTracker.js
@@ -2,4 +2,24 @@ import loadFile from '../../../lib/loadFile.js';
 
 loadFile('week-tracker', '/views/newYear/weekTracker/weekTracker.html');
 
-fetch('/data').then(res => res.json()).then(console.log);
+const getWeekNumber = () => {
+  // stack overflow code....I am lazy.
+  d = new Date();
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0 , 1));
+  let weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  return [d.getUTCFullYear(), weekNo];
+}
+
+fetch('/data').then(res => res.json()).then((payload => {
+  const { data } = payload;
+  const weekOfTheYear = 0;
+
+  for (let key in data) {
+    document.querySelector(`[key="${key}"]`);
+    // get current week of year
+    // get array of index of that week
+    // fill in data accordingly
+  }
+   
+}));
