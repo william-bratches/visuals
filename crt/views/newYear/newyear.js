dde8b289653f82e8fabc8bf3db53a680adaa654e
william-bratches <william.bratches@gmail.com>
2019-01-08 20:43:01 -0500

    fix broken date

--- crt/views/newYear/newyear.js
+++ crt/views/newYear/newyear.js
@@ -2,9 +2,18 @@ const getDate = () => new Date().toUTCString();
 
 const getEpoch = () => Math.floor(new Date().getTime() / 1000);
 
+const dayOfYear = (now) => {
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay);
+  return day;
+};
+
 const getDaysRemaining = () => {
     const newYear = Math.floor(new Date('2019').getTime() / 1000);
-    return 364 - (new Date((newYear - getEpoch()) * 1000).getUTCDay());
+    const currentDay = dayOfYear(new Date());
+    return 364 - currentDay;
 
 }
 
