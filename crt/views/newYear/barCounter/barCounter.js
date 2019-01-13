432fd290dcaa7e3d9fdb5ac6cafc9a274cbd5b9d
william-bratches <william.bratches@gmail.com>
2019-01-12 19:44:02 -0500

    moar refactoring

--- crt/views/newYear/barCounter/barCounter.js
+++ crt/views/newYear/barCounter/barCounter.js
@@ -1,18 +1,22 @@
 import { loadAndHydrate } from '../../../lib/loadFile.js';
 import getProperty from '../../../lib/styles.js';
 import setLabels from '../../common/label.js';
+import setSuccessRate from '../../common/successRate.js';
 
 const className = 'bar-counter';
+
+const expandProgressBar = (el, successRates) => {
+  const width = parseInt(getProperty('.load-box', 'width'));
+  const barWidth = width * successRates;
+  el.querySelector('.load-fill-box').style.width = `${barWidth}px`;
+};
 
 loadAndHydrate('bar-counter', '/views/newYear/barCounter/barCounter.html', (el, payload) => {
   const { data, successRates } = payload;
   const fractionDisplay = `${data.current}/${data.goal}`;
   el.querySelector('.fraction').innerHTML = fractionDisplay;
-
+  setSuccessRate(el, successRates);
   setLabels(el);
 
-  // expand bar to relevants size
-  const width = parseInt(getProperty('.load-box', 'width'));
-  const barWidth = width * successRates;
-  el.querySelector('.load-fill-box').style.width = `${barWidth}px`;
+  expandProgressBar(el, successRates);
 });
