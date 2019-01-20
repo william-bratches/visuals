3816ecd912a9e382770d964e180914928f614787
william-bratches <william.bratches@gmail.com>
2019-01-20 11:51:01 -0500

    data update

--- server/data.js
+++ server/data.js
@@ -3,12 +3,12 @@ const data = () => ({
   coding: [
     { mon: true, tue: false, wed: false, thu: false, fri: false, sat: false, sun: true },
     { mon: false, tue: true, wed: true, thu: true, fri: false, sat: true, sun: false },
-    { mon: true, tue: false, wed: false, thu: false, fri: true, sat: false, sun: false },
+    { mon: true, tue: false, wed: false, thu: false, fri: true, sat: false, sun: true },
   ],
   meditation: [
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
-    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, sun: false },
   ],
   earlybird: [
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
@@ -18,7 +18,7 @@ const data = () => ({
   hair: [
     { mon: false, tue: true, wed: true, thu: true, fri: false, sat: false, sun: false },
     { mon: true, tue: false, wed: false, thu: true, fri: false, sat: true, sun: false },
-    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, sun: false },
   ],
   friends: {
     current: 3,
@@ -41,7 +41,7 @@ const data = () => ({
     goal: 50
   },
   salad: {
-    current: 5,
+    current: 7,
     goal: 200
   },
   opensource: {
