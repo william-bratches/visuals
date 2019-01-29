f44751d6b23a3e8527604371dc7155c2f758916a
william-bratches <william.bratches@gmail.com>
2019-01-28 21:17:28 -0500

    data update

--- server/data.js
+++ server/data.js
@@ -5,7 +5,7 @@ const data = () => ({
     { mon: false, tue: true, wed: true, thu: true, fri: false, sat: true, sun: false },
     { mon: true, tue: false, wed: false, thu: false, fri: true, sat: false, sun: true },
     { mon: true, tue: true, wed: false, thu: false, fri: true, sat: false, sun: true },
-    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: true, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
   ],
   meditation: [
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
