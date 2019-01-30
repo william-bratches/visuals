ad1acbf558e0273e5d2d783ef0c3a429e06c2240
william-bratches <william.bratches@gmail.com>
2019-01-29 19:15:14 -0500

    update

--- server/data.js
+++ server/data.js
@@ -5,14 +5,14 @@ const data = () => ({
     { mon: false, tue: true, wed: true, thu: true, fri: false, sat: true, sun: false },
     { mon: true, tue: false, wed: false, thu: false, fri: true, sat: false, sun: true },
     { mon: true, tue: true, wed: false, thu: false, fri: true, sat: false, sun: true },
-    { mon: true, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: true, tue: true, wed: false, thu: false, fri: false, sat: false, sun: false },
   ],
   meditation: [
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, sun: true },
     { mon: true, tue: false, wed: false, thu: false, fri: true, sat: false, sun: true },
-    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: false, tue: true, wed: false, thu: false, fri: false, sat: false, sun: false },
   ],
   earlybird: [
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
@@ -25,7 +25,7 @@ const data = () => ({
     { mon: true, tue: false, wed: false, thu: true, fri: false, sat: true, sun: false },
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, sun: true },
     { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
-    { mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false },
+    { mon: false, tue: true, wed: false, thu: false, fri: false, sat: false, sun: false },
   ],
   friends: {
     current: 3,
